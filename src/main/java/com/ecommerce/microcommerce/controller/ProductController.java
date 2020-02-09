999969ffb404aeb08ba5ed1982cb6982075c6aa2
Meta <gael.lehchibi@outlook.com>
2020-02-09 12:19:04 +0100

    Ajout du logger, test des méthodes rest avec postman

--- src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
@@ -2,14 +2,14 @@ package com.ecommerce.microcommerce.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class ProductController {
 
     @Autowired
@@ -17,11 +17,19 @@ public class ProductController {
 
     @GetMapping("/Produits")
     public List<Product> listeProduits() {
+        log.info("Retourne la liste des produits");
         return productDao.findAll();
     }
 
     @GetMapping("/Produits/{id}")
     public Product afficherUnProduit(@PathVariable int id) {
+        log.info("Affiche un produit");
         return productDao.findById(id);
+    }
+
+    @PostMapping("/Produits")
+    public void ajouterProduit(@RequestBody Product product) {
+        log.info("Sauvegarde un produit");
+        productDao.save(product);
     }
 }
